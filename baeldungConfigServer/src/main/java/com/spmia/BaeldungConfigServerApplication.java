a513609ffd4409dab80949275ef371afc725aee9
AmrMagdy <a.magdyabdelaal@gmail.com>
2020-05-26 16:17:25 +0400

    config + euraka
    config + euraka

---
+++ baeldungConfigServer/src/main/java/com/spmia/BaeldungConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.spmia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BaeldungConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungConfigServerApplication.class, args);
+	}
+
+}
---
+++ baeldungConfigServer/src/test/java/com/spmia/BaeldungConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spmia;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
